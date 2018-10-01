d478ba060c3e31a53db10d03acb6625ed491542e
Jakub Nurski <nurskijakub@gmail.com>
2018-10-01 22:09:06 +0200

    Added CORS.

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'uopmri7pxk3vd^m1oh^jib#r!sch4i&06g-lkv2=rt#r)aj2p4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'tictactoe',
     'games.apps.GamesConfig',
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
